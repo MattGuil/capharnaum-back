4a2cf96f61e4e36e7ff34925973a32190fb36450
MattGuil <guillemin.matt@gmail.com>
2024-11-26 06:56:26 +0100

    add vercel url to cors allowed origins

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ const bodyParser = require('body-parser');
 
 const allowedOrigins = [
   'http://localhost:5173',
-  'http://localhost:4173'
+  'http://localhost:4173',
+  'https://capharnaum.vercel.app'
 ];
 
 app.use(cors({
