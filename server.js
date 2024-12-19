338687d6ccb9ca2aafe6a7c74668689187682284
MattGuil <guillemin.matt@gmail.com>
2024-12-19 15:05:34 +0100

    api and ws run on the port

--- server.js
+++ server.js
@@ -1,38 +1,67 @@
+const fs = require('fs');
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const jwt = require("jsonwebtoken");
+const http = require('http');
+const https = require('https');
+const socketIo = require('socket.io');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+require("dotenv").config({ path: './config/.env' });
+require('./config/db');
 
-const app = express();
+const cert = fs.readFileSync('./alwaysdata-net.pem');
+
+const httpsAgent = new https.Agent({
+	// ca: cert
+	rejectUnauthorized: false
+});
 
 const userRoutes = require('./routes/user.routes');
 const activityRoutes = require('./routes/activity.routes');
 const favoriteRoutes = require('./routes/favorite.routes');
 const participationRoutes = require('./routes/participation.routes');
-const messageRoutes = require ('./routes/message.routes');
+const messageRoutes = require('./routes/message.routes');
 
-require("dotenv").config({ path: './config/.env' });
-require('./config/db');
-
-const bodyParser = require('body-parser');
+const app = express();
 
 const allowedOrigins = [
-	'http://localhost:5173',
-	'http://localhost:4173',
-	'https://capharnaum.vercel.app'
+    'http://localhost:5173',
+    'http://localhost:4173',
+    'https://capharnaum.vercel.app'
 ];
 
 app.use(cors({
-	origin: function (origin, callback) {
-		if (!origin || allowedOrigins.includes(origin)) {
-			callback(null, true);
-		} else {
-			callback(new Error('Not allowed by CORS'));
-		}
-	},
-	methods: ['GET', 'POST', 'PUT', 'DELETE'],
-	credentials: true
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
 }));
+
+app.use(cookieParser(process.env.TOKEN_SECRET));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Vérification de la session JWT
+app.get('/auth/verify-session', (req, res) => {
+    const token = req.cookies.token;
+    if (token) {
+        jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
+            if (err) {
+                return res.status(401).json({ authenticated: false });
+            }
+            res.status(200).json({ authenticated: true });
+        });
+    } else {
+        res.status(402).json({ authenticated: false });
+    }
+});
 
 app.get('/geocode', async (req, res) => {
     const address = req.query.address;
@@ -48,90 +77,63 @@ app.get('/geocode', async (req, res) => {
     }
 });
 
-app.use(cookieParser(process.env.TOKEN_SECRET));
-
-app.get('/auth/verify-session', (req, res) => {
-	const token = req.cookies.token;
-
-	if (token) {
-		jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
-			if (err) {
-				return res.status(401).json({ authenticated: false });
-			}
-			res.status(200).json({ authenticated: true });
-		});
-	} else {
-		res.status(402).json({ authenticated: false });
-	}
-});
-
 const { Disciplines, ActivityTypes, Days } = require('./enums');
-
 app.get('/enums', (req, res) => {
-	res.json({ Disciplines, ActivityTypes, Days });
+    res.json({ Disciplines, ActivityTypes, Days });
 });
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api/user', userRoutes);
 app.use('/api/activity', activityRoutes);
 app.use('/api/favorite', favoriteRoutes);
 app.use('/api/participation', participationRoutes);
-app.use('/api/message', messageRoutes)
-
-app.listen(process.env.PORT, () => {
-	console.log(`Listening on port ${process.env.PORT} ...`);
-});
-
-
-const axios = require('axios');
-const http = require('http');
-const socketIo = require('socket.io');
+app.use('/api/message', messageRoutes);
 
 const server = http.createServer(app);
+
 const io = socketIo(server, {
-	cors: {
-		origin: '*',
-		methods: ['GET', 'POST'],
-	}
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+    }
 });
 
 io.on('connection', (socket) => {
-	console.log('Un utilisateur est connecté: ', socket.id);
+    console.log('Un utilisateur est connecté: ', socket.id);
 
-	socket.on('joinConversation', (conversationId) => {
-		socket.join(conversationId);
-		console.log(`Un utilisateur a rejoint la conversation : ${conversationId}`);
-	});
+    socket.on('joinConversation', (conversationId) => {
+        socket.join(conversationId);
+        console.log(`Un utilisateur a rejoint la conversation : ${conversationId}`);
+    });
 
-	socket.on('sendMessage', async (data) => {
-		try {
-			const response = await axios.post('http://localhost:5001/api/message/send', {
-				from: data.from,
-				to: data.to,
-				content: data.content,
+    socket.on('sendMessage', async (data) => {
+        try {
+            const response = await axios.post('https://capharnaum.alwaysdata.net/api/message/send', {
+                from: data.from,
+                to: data.to,
+                content: data.content,
+            }, {
+				httpsAgent
 			});
 
-			if (response.data.success) {
-				io.to(data.conversationId).emit('newMessage', {
-					message: response.data.message
-				});
+            if (response.data.success) {
+                io.to(data.conversationId).emit('newMessage', {
+                    message: response.data.message
+                });
+                console.log('Message envoyé avec succès');
+            } else {
+                console.error('Erreur lors de l\'enregistrement du message');
+            }
+        } catch (err) {
+            console.error("Erreur lors de l'envoi du message: ", err);
+        }
+    });
 
-				console.log('Message envoyé avec succès');
-			} else {
-				console.error('Erreur lors de l\'enregistrement du message');
-			}
-		} catch (err) {
-			console.error("Erreur lors de l'envoi du message: ", err);
-		}
-	});
-
-	socket.on('disconnect', () => {
-		console.log("Un utilisateur s'est déconnecté");
-	});
+    socket.on('disconnect', () => {
+        console.log("Un utilisateur s'est déconnecté");
+    });
 });
 
-server.listen(5002, () => {
-	console.log(`Serveur démarré sur le port 5002`);
+const port = process.env.PORT;
+server.listen(port, () => {
+    console.log(`Serveur démarré sur le port ${port}`);
 });
